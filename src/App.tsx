83556936831da56b25ba5605381bde8dd842bd84
Igor C. Batista <igor.batista@gmail.com>
2023-10-11 15:11:24 -0300

    message server is running

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-
+import { useEffect, useState } from 'react';
 import { AppRoutes } from './routes';
+import { api } from './server/axios';
 
 export function App() {
+  const [isRunning, setIsRunning] = useState("")
+
+  useEffect(() => {
+    api.get('/')
+      .then(response => {
+        setIsRunning(response.data.message)
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+
+  }, [])
+
   return (
     <div>
       <AppRoutes />
+      <h2>Server: {isRunning}</h2>
     </div>
   )
 }
